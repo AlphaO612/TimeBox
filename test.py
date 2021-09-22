fedbc57635b4407fe7665053118deb8e1ff4d7ef
AlphaO612 <alphaste08@gmail.com>
2021-09-23 01:02:56 +0300

    Fix bugs in solo.json

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from uuid import uuid4
 from time import sleep
 import datetime, json, os, codecs
 
-pwd = ''#'/root/TimeBox/'
+pwd = '/root/TimeBox/'
 sysTokens = {}
 lib = {
     "mon":"понедельник",
